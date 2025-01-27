5a25abb57a5e8a5753cbcad932fed16d8c2e8dff
Ckp <your-email@example.com>
2025-01-28 00:59:33 +0530

    Add initial project files and structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+
+
+setup( 
+name = 'mlroject',
+version = '0.0.1',
+author = 'Ckp',
+author_email='ckpradhit@mail.com',
+packages=find_packages(),
+install_requires = ['pandas','numpy','seaborn']
+
+)
